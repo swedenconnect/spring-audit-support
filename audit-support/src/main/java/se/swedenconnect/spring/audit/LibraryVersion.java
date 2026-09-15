6f1f68d516a2f645e4b032b6796bc130c5e2a35c
Martin Lindström <martin-lindstrom@users.noreply.github.com>
2026-09-15 17:47:42 +0200

    build: 1.0.0 release (#4)
    * build: 1.0.0
    
    * build: Bumped version after 1.0.0 release

--- audit-support/src/main/java/se/swedenconnect/spring/audit/LibraryVersion.java
+++ audit-support/src/main/java/se/swedenconnect/spring/audit/LibraryVersion.java
@@ -26,7 +26,7 @@ public final class LibraryVersion {
 
   private static final int MAJOR = 1;
   private static final int MINOR = 0;
-  private static final int PATCH = 0;
+  private static final int PATCH = 1;
 
   /**
    * Global serialization value for Audit Support classes.
